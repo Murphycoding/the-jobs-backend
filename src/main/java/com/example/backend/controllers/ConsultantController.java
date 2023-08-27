988815559c646cc9150973eb843efb0f003c8990
Nisal Tharuka <nisaltharuka98@gmail.com>
2023-08-27 18:10:28 +0530

    add save function to consultant controller

--- src/main/java/com/example/backend/controllers/ConsultantController.java
+++ src/main/java/com/example/backend/controllers/ConsultantController.java
@@ -1,20 +1,60 @@
 package com.example.backend.controllers;
 
+import com.example.backend.models.Consultant;
+import com.example.backend.models.JobSeeker;
+import com.example.backend.models.User;
+import com.example.backend.payload.request.ConsultantRequest;
+import com.example.backend.payload.request.JobSeekerRequest;
+import com.example.backend.repository.ConsultantRepository;
+import com.example.backend.repository.UserRepository;
+import com.example.backend.security.jwt.JwtUtils;
+import com.example.backend.security.services.UserDetailsServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/consultant")
 public class ConsultantController {
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ConsultantRepository consultantRepository;
+
     @GetMapping("/dashboard")
     @PreAuthorize("hasRole('CONSULTANT')")
     public Object dashboard(HttpServletRequest request) {
 
         return null;
+    }
+    @PostMapping("/save")
+    @PreAuthorize("hasRole('CONSULTANT')")
+    public Object save(HttpServletRequest request , @RequestBody ConsultantRequest consultantRequest){
+
+        String token = jwtUtils.getJwtFromCookies(request);
+        String username = jwtUtils.getUserNameFromJwtToken(token);
+        Optional<User> user = userRepository.findByUsername(username);
+
+        Consultant consultant = new Consultant(
+                consultantRequest.getSpecialized_area(),
+                consultantRequest.getSpecialized_country(),
+                consultantRequest.getTp(),
+                consultantRequest.getAddress(),
+                consultantRequest.getDob(),
+                consultantRequest.getNic(),
+                consultantRequest.getGender(),
+                user.orElse(new User())
+        );
+        return consultantRepository.save(consultant);
     }
 
 }
--- src/main/java/com/example/backend/payload/request/ConsultantRequest.java
+++ src/main/java/com/example/backend/payload/request/ConsultantRequest.java
@@ -1,55 +1,25 @@
-package com.example.backend.models;
+package com.example.backend.payload.request;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.lang.String;
-import java.util.HashSet;
-import java.util.Set;
 
-@Entity
-@Table(name = "consultants",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "tp"),
-                @UniqueConstraint(columnNames = "nic")
-        })
-public class Consultant {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class ConsultantRequest {
     @NotBlank
-    @Size(max = 255)
     private String specialized_area;
     @NotBlank
-    @Size(max = 255)
     private String specialized_country;
     @NotBlank
-    @Size(max = 15)
     private String tp;
     @NotBlank
-    @Size(max = 255)
     private String address;
     @NotBlank
     private String dob;
     @NotBlank
-    @Size(max = 12)
     private String  nic;
     @NotBlank
-    @Size(max = 10)
     private String  gender;
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getSpecialized_area() {
         return specialized_area;
@@ -105,27 +75,5 @@ public class Consultant {
 
     public void setGender(String gender) {
         this.gender = gender;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Consultant() {
-    }
-
-    public Consultant(String specialized_area, String specialized_country, String tp, String address, String dob, String nic, String gender, User user) {
-        this.specialized_area = specialized_area;
-        this.specialized_country = specialized_country;
-        this.tp = tp;
-        this.address = address;
-        this.dob = dob;
-        this.nic = nic;
-        this.gender = gender;
-        this.user = user;
     }
 }
