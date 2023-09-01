ce414c93a79d436d0918e1284a6577eaecf82858
Nisal Tharuka <nisaltharuka98@gmail.com>
2023-09-01 14:10:05 +0530

    job seeker dashboard send current consultant

--- src/main/java/com/example/backend/controllers/JobSeekerController.java
+++ src/main/java/com/example/backend/controllers/JobSeekerController.java
@@ -1,16 +1,19 @@
 package com.example.backend.controllers;
 
+import com.example.backend.models.Consultant;
 import com.example.backend.models.JobSeeker;
 import com.example.backend.models.User;
 import com.example.backend.payload.request.JobSeekerRequest;
 import com.example.backend.repository.JobSeekerRepository;
 import com.example.backend.repository.UserRepository;
 import com.example.backend.security.jwt.JwtUtils;
+import com.example.backend.security.services.UserDetailsImpl;
 import com.example.backend.security.services.UserDetailsServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -31,8 +34,11 @@ public class JobSeekerController {
     @GetMapping("/dashboard")
     @PreAuthorize("hasRole('jOB_SEEKER')")
     public Object dashboard(HttpServletRequest request) {
-
-        return null;
+        String token = jwtUtils.getJwtFromCookies(request);
+        String username = jwtUtils.getUserNameFromJwtToken(token);
+        Optional<User> user = userRepository.findByUsername(username);
+        Optional<JobSeeker> jobSeeker = jobSeekerRepository.findByUser(user.orElse(new User()));
+        return jobSeeker;
     }
     @PostMapping("/save")
     @PreAuthorize("hasRole('jOB_SEEKER')")
--- src/main/java/com/example/backend/repository/JobSeekerRepository.java
+++ src/main/java/com/example/backend/repository/JobSeekerRepository.java
@@ -1,9 +1,12 @@
 package com.example.backend.repository;
 
+import com.example.backend.models.Consultant;
 import com.example.backend.models.JobSeeker;
+import com.example.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface JobSeekerRepository extends JpaRepository<JobSeeker, Long> {
+    Optional<JobSeeker> findByUser(User user);
 }
